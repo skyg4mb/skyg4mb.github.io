466d24bab5858a3b58be714b5b2166690e6e73bd
skyg4mb <diego.a.gamboa.p@gmail.com>
2021-04-18 15:50:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -82,3 +82,29 @@ var Typer={
 			this.write("|"); // else write it
 	}
 }
+function replaceUrls(text) {
+	var http = text.indexOf("http://");
+	var space = text.indexOf(".me ", http);
+	
+	if (space != -1) { 
+		var url = text.slice(http, space-1);
+		return text.replace(url, "<a href=\""  + url + "\">" + url + "</a>");
+	} 
+	
+	else {
+		return text
+	}
+}
+
+Typer.speed=3;
+Typer.file="[your-name].txt"; // add your own name here
+Typer.init();
+ 
+var timer = setInterval("t();", 30);
+function t() {
+	Typer.addText({"keyCode": 123748});
+	
+	if (Typer.index > Typer.text.length) {
+		clearInterval(timer);
+	}
+}
