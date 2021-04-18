5f913a41a24cc1bdb363807bcdf4c6ee7a96a372
skyg4mb <diego.a.gamboa.p@gmail.com>
2021-04-18 15:53:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed=3;
-Typer.file="[your-name].txt"; // add your own name here
+Typer.file="skyg4mb.txt"; // add your own name here
 Typer.init();
  
 var timer = setInterval("t();", 30);
